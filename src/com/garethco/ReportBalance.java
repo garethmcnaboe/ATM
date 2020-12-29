71d82d91d1d8d95756a96347957e9dcf4d2338ad
Gareth McNaboe <gareth.mcnaboe.2021@mumail.com>
2020-12-29 21:15:33 +0000

    Commit 3

--- src/com/garethco/ReportBalance.java
+++ src/com/garethco/ReportBalance.java
@@ -4,15 +4,14 @@ import java.text.NumberFormat;
 
 public class ReportBalance {
 
-    private static final NumberFormat currency = null;
-
     public static void printBalance(double balance, double availableFunds) {
-        //String closingBalanceFormatted = currency.format(balance);
-        //String availableFundsFormatted = currency.format(availableFunds);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String balanceFormatted = currency.format(balance);
+        String availableFundsFormatted = currency.format(availableFunds);
         System.out.println("Account Balance");
         System.out.println("______________________________________");
-        System.out.println("Your balance is: " + balance);
+        System.out.println("Your balance is: " + balanceFormatted);
         System.out.println("______________________________________");
-        System.out.println("Your Available Funds are " + availableFunds);
+        System.out.println("Your Available Funds are " + availableFundsFormatted);
     }
 }
